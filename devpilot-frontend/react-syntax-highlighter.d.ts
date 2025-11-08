5b41ec711a21e808b3176f81277e47bf7ab051f4
Sohan Mavji <sohan.m0904@gmail.com>
2025-11-08 11:59:10 +0000

    Add missing hljs style module declaration

--- devpilot-frontend/react-syntax-highlighter.d.ts
+++ devpilot-frontend/react-syntax-highlighter.d.ts
@@ -3,4 +3,5 @@ declare module "react-syntax-highlighter/dist/esm/languages/hljs/javascript";
 declare module "react-syntax-highlighter/dist/esm/languages/hljs/typescript";
 declare module "react-syntax-highlighter/dist/esm/languages/hljs/python";
 declare module "react-syntax-highlighter/dist/esm/languages/hljs/json";
+declare module "react-syntax-highlighter/dist/esm/styles/hljs";
 declare module "react-syntax-highlighter/dist/esm/styles/hljs/atom-one-dark";
